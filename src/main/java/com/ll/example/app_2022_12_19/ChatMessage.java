99803a2bee4b9981f920db990eab185c56ae0d21
dohyu <dohyunre702@gmail.com>
2022-12-19 10:49:36 +0900

    [add] /chat/messages - messages get api 작성 - 작성내용 json 전달 - (record) java version upgrade

--- src/main/java/com/ll/example/app_2022_12_19/ChatMessage.java
+++ src/main/java/com/ll/example/app_2022_12_19/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.ll.example.app_2022_12_19;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,12 +10,13 @@ import java.time.LocalDateTime;
 @Getter
 public class ChatMessage {
     private long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createDate;
     private String authorName;
     private String content;
 
     public ChatMessage(String authorName, String content) {
-        this(1, LocalDateTime.now(), authorName, content);
+        this(ChatMessageIdGenerator.genNextId(), LocalDateTime.now(), authorName, content);
     }
 }
 
--- src/main/java/com/ll/example/app_2022_12_19/controller/ChatController.java
+++ src/main/java/com/ll/example/app_2022_12_19/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.ll.example.app_2022_12_19.controller;
 import com.ll.example.app_2022_12_19.ChatMessage;
 import com.ll.example.app_2022_12_19.RsData;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,17 +15,34 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatController {
     private List<ChatMessage> chatMessages = new ArrayList<>();
+    public static class WriteMessgaeRequest() {
+        private final String authorName;
+        private final String content;
+    }
 
+    public record WriteMessageResponse(long id) { //jdk 16+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<ChatMessage> writeMessage() {
+    public RsData<WriteMessageResponse> writeMessage() {
         ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
 
-        return new RsData(
+        chatMessages.add(message);
+
+        return new RsData<>(
                 "S-1",
                 "메세지가 작성되었습니다.",
-                null);
+                new WriteMessageResponse(message.getId()));
+    }
+
+    @GetMapping("/messages") //메세지 가져오기
+    @ResponseBody
+    public RsData<List<ChatMessage>> messages() {
+        return new RsData<>(
+                "S-1",
+                "성공",
+                chatMessages);
     }
 
 }
