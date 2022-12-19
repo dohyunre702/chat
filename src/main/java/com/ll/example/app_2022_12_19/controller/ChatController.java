b6565d425662129092f8a82f37448ee9b7dc7001
dohyu <dohyunre702@gmail.com>
2022-12-19 10:16:56 +0900

    [add] 메시지 작성 서버 포트 할당 POST. 메시지 작성

---
+++ src/main/java/com/ll/example/app_2022_12_19/controller/ChatController.java
@@ -0,0 +1,18 @@
+package com.ll.example.app_2022_12_19.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public String writeMessage() {
+        return "메세지가 작성되었습니다.";
+    }
+
+}
