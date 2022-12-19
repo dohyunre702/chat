ba82ba73d5a18665153983d6f7aa2538cba5d907
dohyu <dohyunre702@gmail.com>
2022-12-19 15:40:31 +0900

    [add] - update: 채팅방, 채팅메세지 작성폼 - SSE 적용

---
+++ src/main/java/com/ll/example/app_2022_12_19/Ut.java
@@ -0,0 +1,24 @@
+package com.ll.example.app_2022_12_19;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Ut {
+    public static <K, V> Map<K, V> mapOf(Object... args) {
+        Map<K, V> map = new LinkedHashMap<>();
+
+        int size = args.length/2;
+
+        for(int i = 0; i < size; i++) {
+            int keyIndex = i * 2;
+            int valueIndex = keyIndex + 1;
+
+            K key = (K) args[keyIndex];
+            V value = (V) args[valueIndex];
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+}
---
+++ src/main/java/com/ll/example/app_2022_12_19/controller/SseController.java
@@ -0,0 +1,4 @@
+package com.ll.example.app_2022_12_19.controller;
+
+public class SseController {
+}
