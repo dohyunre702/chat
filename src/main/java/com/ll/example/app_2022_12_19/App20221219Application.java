429b1a74e429416ff43c7a15d966cc435d76b2dc
dohyu <dohyunre702@gmail.com>
2022-12-19 10:04:22 +0900

    Initial commit

---
+++ src/main/java/com/ll/example/app_2022_12_19/App20221219Application.java
@@ -0,0 +1,13 @@
+package com.ll.example.app_2022_12_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20221219Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App20221219Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/example/app_2022_12_19/App20221219ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.example.app_2022_12_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20221219ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
